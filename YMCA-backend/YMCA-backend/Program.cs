3eab8c9e78e55c71bd052e0a90e8359e49cce963
kimkristianssonJU <krki21cn@student.ju.se>
2022-09-12 15:18:21 +0200

    First endpoint done

--- YMCA-backend/YMCA-backend/Program.cs
+++ YMCA-backend/YMCA-backend/Program.cs
@@ -1,14 +1,43 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<CookiePolicyOptions>(options => {
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+    options.HandleSameSiteCookieCompatibility();
+});
+
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services
+  .Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme,
+    options => {
+        options.Events = new OpenIdConnectEvents {
+            // When the token is validated, store it as a claim so that it can.be retrieved 
+            // from the user object later 
+            OnTokenValidated = ctx => {
+                var jwtToken = ctx.SecurityToken.RawData;
+
+                if (ctx.Principal?.Identity != null) {
+                    ((ClaimsIdentity)ctx.Principal.Identity).AddClaim(new Claim("jwtToken", jwtToken));
+                    return Task.CompletedTask;
+                }
+
+                throw new NullReferenceException("Ctx.Principal or ctx.Principal.Identity is Null");
+            }
+        };
+    });
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +55,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+
 app.MapControllers();
+
 
 app.Run();
