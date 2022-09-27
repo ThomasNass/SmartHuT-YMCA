9d8184d80a98188e74b39fc97e73168f780a1c4c
kimkristianssonJU <krki21cn@student.ju.se>
2022-09-27 10:13:27 +0200

    Uppdatera historik
    Har troligen fixat så att historik uppdateras varje gång man går in där

--- YMCA/YMCA-frontend/src/Components/History/History.jsx
+++ YMCA/YMCA-frontend/src/Components/History/History.jsx
@@ -1,112 +1,171 @@
-import React, { useEffect, useState } from 'react';
-import { HistoryDisplay, HistoryDisplay } from './HistoryDisplay';
-import { getAll, getToken } from '../../js/script.js';
-import styles from './history.module.css';
-
+import React, { useEffect, useState } from "react";
+import { HistoryDisplay, HistoryDisplay } from "./HistoryDisplay";
+import { getAll, getToken } from "../../js/script.js";
+import styles from "./history.module.css";
 
 const History = (props) => {
-    let [roomOneTemp, setroomOneTemp] = useState([])
-    let [roomTwoTemp, setroomTwoTemp] = useState([])
-    let [roomTwoHum, setroomTwoHum] = useState([])
-    let [roomThreeTemp, setroomThreeTemp] = useState([])
-    let [roomThreeHum, setroomThreeHum] = useState([])
-    let [poolTemp, setPoolTemp] = useState([])
+    let [roomOneTemp, setroomOneTemp] = useState([]);
+    let [roomTwoTemp, setroomTwoTemp] = useState([]);
+    let [roomTwoHum, setroomTwoHum] = useState([]);
+    let [roomThreeTemp, setroomThreeTemp] = useState([]);
+    let [roomThreeHum, setroomThreeHum] = useState([]);
+    let [poolTemp, setPoolTemp] = useState([]);
 
-    let showHistory = props.showClimate ? `${styles.hide}` : null;
+    let showHistory = !props.showClimate ? null : `${styles.hide}`;
 
     const GetTheLog = async (deviceId, token) => {
         try {
-            let response = await fetch(`https://api.smarthut.se/DeviceInfo/GetAlarmLogs/${deviceId}`, {
-                headers: {
-                    "Authorization": `Bearer ${token}`
+            let response = await fetch(
+                `https://api.smarthut.se/DeviceInfo/GetAlarmLogs/${deviceId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
                 }
-            })
-            let data = await response.json();;
+            );
+            let data = await response.json();
             return data;
+        } catch (e) {
+            console.log(e);
         }
-        catch (e) {
-            console.log(e)
-        }
-    }
+    };
 
     // because of async I moved them inside use Effect
     useEffect(() => {
-
-        const GetHistoryForRoom = async () => {
-            let buildingWDevices = await getAll();
-            let devicesInRoom = buildingWDevices.devices;
-            let token = await getToken();
-            for (let i = 0; i < devicesInRoom.length; i++) {
-                let historyInfo = await GetTheLog(devicesInRoom[i].id, token)
-                for (let j = 0; j < historyInfo.length; j++) {
-                    if (devicesInRoom[i].id == historyInfo[j].deviceId) {
-                        var date = new Date(historyInfo[j].timeStamp);
-                        justTime = "Kl:" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " ";
-                        if (devicesInRoom[i].name.includes("Temperature Conference room 1")) {
-                            roomOneTemp.push(justTime)
-                        }
-                        if (devicesInRoom[i].name.includes("Temperature Conference room 2")) {
-                            roomTwoTemp.push(justTime)
-                        }
-                        if (devicesInRoom[i].name.includes("Humidity Conference room 2")) {
-                            roomTwoHum.push(justTime)
-                        }
-                        if (devicesInRoom[i].name.includes("Temperature Conference room 3")) {
-                            roomThreeTemp.push(justTime)
-                        }
-                        if (devicesInRoom[i].name.includes("Humidity Conference room 3")) {
-                            roomThreeHum.push(justTime)
-                        }
-                        if (devicesInRoom[i].name.includes("Temperature swimming pool 1")) {
-                            poolTemp.push(justTime)
+        if (!props.showClimate) {
+            const GetHistoryForRoom = async () => {
+                let buildingWDevices = await getAll();
+                let devicesInRoom = buildingWDevices.devices;
+                let token = await getToken();
+                for (let i = 0; i < devicesInRoom.length; i++) {
+                    let historyInfo = await GetTheLog(
+                        devicesInRoom[i].id,
+                        token
+                    );
+                    for (let j = 0; j < historyInfo.length; j++) {
+                        if (devicesInRoom[i].id == historyInfo[j].deviceId) {
+                            var date = new Date(historyInfo[j].timeStamp);
+                            justTime =
+                                "Kl:" +
+                                date.getHours() +
+                                ":" +
+                                date.getMinutes() +
+                                ":" +
+                                date.getSeconds() +
+                                " ";
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Temperature Conference room 1"
+                                )
+                            ) {
+                                roomOneTemp.push(justTime);
+                            }
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Temperature Conference room 2"
+                                )
+                            ) {
+                                roomTwoTemp.push(justTime);
+                            }
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Humidity Conference room 2"
+                                )
+                            ) {
+                                roomTwoHum.push(justTime);
+                            }
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Temperature Conference room 3"
+                                )
+                            ) {
+                                roomThreeTemp.push(justTime);
+                            }
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Humidity Conference room 3"
+                                )
+                            ) {
+                                roomThreeHum.push(justTime);
+                            }
+                            if (
+                                devicesInRoom[i].name.includes(
+                                    "Temperature swimming pool 1"
+                                )
+                            ) {
+                                poolTemp.push(justTime);
+                            }
                         }
                     }
-
                 }
-            }
-            const listRoomOneTemp = roomOneTemp.map((time) =>
-                <span>{time}</span>
-            ).slice(0, 10).sort()
-            setroomOneTemp(roomOneTemp => [...roomOneTemp, listRoomOneTemp]);
-            const listRoomTwoTemp = roomTwoTemp.map((time) =>
-                <span>{time}</span>
-            ).sort()
-            setroomTwoTemp(roomTwoTemp => [...roomTwoTemp, listRoomTwoTemp]);
-            const listRoomTwoHum = roomTwoHum.map((time) =>
-                <span>{time}</span>
-            ).sort()
-            setroomTwoHum(roomTwoHum => [...roomTwoHum, listRoomTwoHum]);
+                const listRoomOneTemp = roomOneTemp
+                    .map((time) => <span>{time}</span>)
+                    .slice(0, 10)
+                    .sort();
+                setroomOneTemp((roomOneTemp) => [
+                    ...roomOneTemp,
+                    listRoomOneTemp,
+                ]);
+                const listRoomTwoTemp = roomTwoTemp
+                    .map((time) => <span>{time}</span>)
+                    .sort();
+                setroomTwoTemp((roomTwoTemp) => [
+                    ...roomTwoTemp,
+                    listRoomTwoTemp,
+                ]);
+                const listRoomTwoHum = roomTwoHum
+                    .map((time) => <span>{time}</span>)
+                    .sort();
+                setroomTwoHum((roomTwoHum) => [...roomTwoHum, listRoomTwoHum]);
 
-            const listRoomThreeTemp = roomThreeTemp.map((time) =>
-                <span>{time}</span>
-            ).sort()
-            setroomThreeTemp(roomThreeTemp => [...roomThreeTemp, listRoomThreeTemp]);
-            const listRoomThreeHum = roomThreeHum.map((time) =>
-                <span>{time}</span>
-            ).sort()
-            setroomThreeHum(roomThreeHum => [...roomThreeHum, listRoomThreeHum]);
+                const listRoomThreeTemp = roomThreeTemp
+                    .map((time) => <span>{time}</span>)
+                    .sort();
+                setroomThreeTemp((roomThreeTemp) => [
+                    ...roomThreeTemp,
+                    listRoomThreeTemp,
+                ]);
+                const listRoomThreeHum = roomThreeHum
+                    .map((time) => <span>{time}</span>)
+                    .sort();
+                setroomThreeHum((roomThreeHum) => [
+                    ...roomThreeHum,
+                    listRoomThreeHum,
+                ]);
 
-            const listPoolTemp = poolTemp.map((time) =>
-                <span>{time}</span>
-            ).sort()
-            setPoolTemp(poolTemp => [...poolTemp, listPoolTemp]);
+                const listPoolTemp = poolTemp
+                    .map((time) => <span>{time}</span>)
+                    .sort();
+                setPoolTemp((poolTemp) => [...poolTemp, listPoolTemp]);
+            };
+            GetHistoryForRoom();
         }
-        GetHistoryForRoom();
-    }, []);
-
-
+    }, [props.showClimate]);
 
     return (
-
         <div className={showHistory}>
-            <HistoryDisplay room={"Konferensrum 1"} Temp={roomOneTemp.slice(0, 10)} Hum={null} />
-            <HistoryDisplay room={"Konferensrum 2"} Temp={roomTwoTemp.slice(0, 10)} Hum={roomTwoHum.slice(0, 10)} />
-            <HistoryDisplay room={"Konferensrum 3"} Temp={roomThreeTemp.slice(0, 10)} Hum={roomThreeHum.slice(0, 10)} />
-            <HistoryDisplay room={"Pool"} Temp={poolTemp.slice(0, 10)} Hum={null} />
+            <HistoryDisplay
+                room={"Konferensrum 1"}
+                Temp={roomOneTemp.slice(0, 10)}
+                Hum={null}
+            />
+            <HistoryDisplay
+                room={"Konferensrum 2"}
+                Temp={roomTwoTemp.slice(0, 10)}
+                Hum={roomTwoHum.slice(0, 10)}
+            />
+            <HistoryDisplay
+                room={"Konferensrum 3"}
+                Temp={roomThreeTemp.slice(0, 10)}
+                Hum={roomThreeHum.slice(0, 10)}
+            />
+            <HistoryDisplay
+                room={"Pool"}
+                Temp={poolTemp.slice(0, 10)}
+                Hum={null}
+            />
         </div>
-
-    )
-
-}
+    );
+};
 
 export default History;
